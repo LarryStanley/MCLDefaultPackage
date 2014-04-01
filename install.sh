af83d8f0abdcc13f53013840c2c97f4e3a5aad37
LarryStanley <kill.xmurderer@gmail.com>
2014-04-01 12:03:11 +0800

    Add upgrade command.

--- install.sh
+++ install.sh
@@ -4,4 +4,5 @@ sudo apt-get install emacs -y
 sudo apt-get install python3-all -y
 sudo apt-get install gnome-genius -y
 sudo apt-get install texlive-xetex -y
+sudo apt-get upgrade -y
 sudo shutdown -h now 
