5fa491744834d7309e3465cf317ce7f845eb4f15
LarryStanley <kill.xmurderer@gmail.com>
2014-04-01 11:59:42 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+sudo apt-get update
+sudo apt-get install emacs -y
+sudo apt-get install python3-all -y
+sudo apt-get install gnome-genius -y
+sudo apt-get install texlive-xetex -y
+sudo shutdown -h now 
