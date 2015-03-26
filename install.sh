24025fb08f6adfc9a220fb83df476e4b55253a16
LarryStanley <kill.xmurderer@gmail.com>
2015-03-26 19:50:16 +0800

    Add idle 3 install.

--- install.sh
+++ install.sh
@@ -4,5 +4,8 @@ sudo apt-get install emacs -y
 sudo apt-get install python3-all -y
 sudo apt-get install gnome-genius -y
 sudo apt-get install texlive-xetex -y
+sudo apt-get install vim -y
+sudo apt-get install git -y
+sudo apt-get install idle3 -y
 sudo apt-get upgrade -y
 sudo shutdown -h now 
