2d10b0b65fa630ba8405b05d670f0bda6f9a3384
LarryStanley <kill.xmurderer@gmail.com>
2015-08-31 14:46:47 +0800

    Add windows package.

---
+++ winAutoForMCL.bat
@@ -0,0 +1,13 @@
+cd Desktop\auotInstall
+ChromeSetup.exe /silent /install
+avira.exe /silent /install
+FileZilla.exe /S
+devcpp.exe /S
+winrar.exe /S
+COPY "putty.exe" %userprofile%\Desktop
+npp.exe /S
+@start /wait Firefox.exe -ms
+flashplayer.exe /S
+chromeinstall.exe
+readerdc.exe /S
+PCMan.exe /S
