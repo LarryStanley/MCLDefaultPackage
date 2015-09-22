a31417974ad66d3d589e4065202aae1017e28dc8
Stanley <kill.xmurderer@gmail.com>
2015-09-22 09:46:43 +0800

    Add pchman and g++

--- install.sh
+++ install.sh
@@ -7,5 +7,7 @@ sudo apt-get install texlive-xetex -y
 sudo apt-get install vim -y
 sudo apt-get install git -y
 sudo apt-get install idle3 -y
+sudo apt-get install pcman.* -y
+sudo apt-get install g++ -y
 sudo apt-get upgrade -y
-sudo shutdown -h now 
+sudo shutdown -h now
